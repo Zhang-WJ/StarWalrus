4b190a76265f8cabe6cb21c5a4d64917e5907d11
zhangweijun <ahnqihyun@sina.cn>
2022-06-15 19:51:44 +0800

    add game menu state

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,16 @@
 package;
 
 import flixel.FlxGame;
+import flixel.FlxState;
 import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+	var initialState:Class<FlxState> = MenuState;
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		addChild(new FlxGame(0, 0, MenuState));
 	}
 }
